f475f77238ab9b940a196332be3324a461ad6b95
Erick Wendel <erick.workspace@hotmail.com>
2017-08-31 04:12:19 -0300

    reverse-proxy

--- exemplo-nginx-reverseproxy-docker/app/index.js
+++ exemplo-nginx-reverseproxy-docker/app/index.js
@@ -3,20 +3,12 @@ var express = require('express'),
     redis = require('redis');
 
 var app = express();
-
-console.log(process.env.REDIS_PORT_6379_TCP_ADDR + ':' + process.env.REDIS_PORT_6379_TCP_PORT);
-
-// APPROACH 1: Using environment variables created by Docker
-// var client = redis.createClient(
-//      process.env.REDIS_PORT_6379_TCP_PORT,
-//      process.env.REDIS_PORT_6379_TCP_ADDR
-// );
-
-// APPROACH 2: Using host entries created by Docker in /etc/hosts (RECOMMENDED)
+ 
 var client = redis.createClient('6379', 'redis');
 
 
 app.get('/', function(req, res, next) {
+  console.log(`Receiving request from: ${process.env.CONTAINER} `);
   client.incr('counter', function(err, counter) {
     if(err) return next(err);
     res.send(`Container: ${process.env.CONTAINER} - This page has been viewed ${ counter }  times!`);
