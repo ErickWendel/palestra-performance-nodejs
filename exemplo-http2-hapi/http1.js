53c1610d9bcadebcd2a75cf07c81d8a3519fed9a
erickwendel <erick.workspace@gmail.com>
2017-09-01 07:05:43 -0300

    log

--- exemplo-http2-hapi/http1.js
+++ exemplo-http2-hapi/http1.js
@@ -11,7 +11,7 @@ const options = {
     cert: fs.readFileSync('certificates/server.crt')
 };
 server.connection({ 
-    address: '0.0.0.0',
+    address: 'localhost',
     port: 4000,
     tls: options
 
@@ -41,4 +41,4 @@ server.register(inert, () => {
     }]);
 })
 
-server.start((err) => log(`Server running at:  `));
+server.start((err) => log(`Server running at:  ${server.info.uri}`));
